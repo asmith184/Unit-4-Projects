cb39b44d203578391d65d8a8cdd107184ba90c31
Aspen <asmith184@west-mec.org>
2020-03-12 14:55:41 -0700

    finished case 1

--- case1/bw_review.js
+++ case1/bw_review.js
@@ -6,7 +6,7 @@
    Case Problem 1
 
    Author:  Aspen Smith
-   Date:   
+   Date:   3-12-2020
    
    Filename: bw_review.js
 	
@@ -35,11 +35,56 @@
 */
 
 
+window.onload = init();
 
-  
+function init(){
+   var stars = document.querySelectorAll("span#stars");
+   for(var i = 0; i< stars.length; i++){
+      stars[i].style.cursor = "pointer";
+      stars[i].addEventListener("mouseenter", lightStars)
+   }
+   document.getElementById("comment").addEventListener("keyup", updateCount);
+}
 
-sdfs
-  
+function lightStars(e){
+   var starNumber = e.target.alt;
+   var stars = document.querySelectorAll("span#stars img");
+   for(var i = 0; i<starNumber.length; i++){
+      stars[i].src = "bw_star2.png";
+   }
+   for(var j = starNumber; j<5; j++){
+      stars[j].src = "bw_star.png";
+   }
+   document.getElementById("rating").value = starNumber + "stars";
+   e.target.addEventListener("mouseleave", turnOffStars);
+
+   e.target.addEventListener("click", function(){
+      e.target.removeEventListener("mouseleave", turnOffStars);      
+   })
+}
+
+function turnOffStars(){
+   var stars = document.querySelectorAll("span#stars img");
+   for(var i = 0; i<stars.length; i++){
+      stars[i].src = "bw_star.png";
+   }
+   document.getElementById("rating") = "";
+}
+
+function updateCount(){
+   var commentText = document.getElementById("comment").value;
+   var charCount = countCharacters(commentText);
+   var wordCountBox = document.getElementById("wordCount");
+   document.getElementById("wordCount").innerHTML = charCount.value + "/1000";
+   if(charCount > 1000){
+      wordCount.style.color = "white";
+      wordCount.style.backgroundColor = "red";
+   }
+   else{
+      wordCount.style.color = "black";
+      wordCount.style.backgroundColor = "white";
+   }
+}  
   
   
 /*=================================================================*/
